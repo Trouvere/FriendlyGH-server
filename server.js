650491a157d139618f45590310b61a0365cee35f
Aliaksei Mishyn <trouvere777@mail.ru>
2021-01-27 16:05:20 +0300

    Add corsOptions in .env

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+
 const apiRouter = require('./app/routes');
 // const db = require('./app/models');
 // const initial = require('./app/data');
 
-const app = express();
-
+const root = path.join.bind('');
+dotenv.config({ path: root('.env.local') });
+// console.log(process.env);
 const corsOptions = {
   // origin: 'http://localhost:3000'
-  origin: 'https://friendlygh.netlify.app'
+  // origin: 'https://friendlygh.netlify.app'
+  origin: process.env.CORS_OPTIONS_ORIGIN
 };
+
+const app = express();
 
 app.use(cors(corsOptions));
 
@@ -23,6 +30,7 @@ app.use('/api/', apiRouter());
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on port ${PORT}.`);
 });
 
